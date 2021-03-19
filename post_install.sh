200afb96cf5a1eab7a1961091ef8b6f9187b6202
fulder <misad90@gmail.com>
2021-03-19 19:29:40 +0100

    Add quotes around key values

--- post_install.sh
+++ post_install.sh
@@ -84,9 +84,9 @@ php artisan passport:client --personal --no-interaction
 
 {
   echo ''
-  echo "PASSPORT_PRIVATE_KEY=$(tr -d '\r\n' < /usr/local/www/monica/storage/oauth-private.key)"
+  echo "PASSPORT_PRIVATE_KEY=\"$(tr -d '\r\n' < /usr/local/www/monica/storage/oauth-private.key)\""
   echo ''
-  echo "PASSPORT_PUBLIC_KEY=$(tr -d '\r\n' < /usr/local/www/monica/storage/oauth-public.key)"
+  echo "PASSPORT_PUBLIC_KEY=\"$(tr -d '\r\n' < /usr/local/www/monica/storage/oauth-public.key)\""
 } >>/usr/local/www/monica/.env
 
 APACHE_USER=www
